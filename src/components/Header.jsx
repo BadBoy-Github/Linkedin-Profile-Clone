122abcaec8b6bbd64bee15e0a6a9bfdc79949d44
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-08 16:40:53 +0530

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+        <div className="bg-red-400 w-full h-16">
+            <div className="flex items-center h-full justify-center text-white px-60">
+                Linkedin
+                Search
+                Home
+                My Network
+                Jobs
+                Messaging
+                Notifications
+                Me
+                For Business
+                Try Premium for free
+            </div>
+        </div>
+    </>
+  )
+};
+
+export default Header;
