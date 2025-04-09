25f1c089e9b17670175ded9d9e66f134a6e5631e
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-10 00:04:19 +0530

    sidebar changes

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,15 +8,19 @@ const SideBar = () => {
         <div className="px-4 py-4" id="top1">
           <div className="border-b-2 border-black pb-4" id="pl">
             <div className="text-[#e9e9e9]">
-              <h1 className="text-lg font-bold">Profile language</h1>
-              <p className="text-sm pt-0.5">English</p>
+              <h1 className="text-[18px] font-semibold">Profile language</h1>
+              <p className="text-[14px] pt-0.5 text-[#8d8f91]">English</p>
             </div>
           </div>
 
           <div className="pt-4" id="ppu">
             <div className="text-[#e9e9e9]">
-              <h1 className="text-lg font-bold">Public Profile & URL</h1>
-              <p className="text-sm pt-0.5">www.linkedin.com/in/elayabarathi</p>
+              <h1 className="text-[18px] font-semibold">
+                Public Profile & URL
+              </h1>
+              <p className="text-[14px] pt-0.5 text-[#8d8f91]">
+                www.linkedin.com/in/elayabarathi
+              </p>
             </div>
           </div>
         </div>
