e32b78dd8d15460fb7c1f6192f6768119a0db82c
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-11 21:53:30 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,13 @@ import React from "react";
 import { FaLinkedinIn } from "react-icons/fa";
 import { HiSearch } from "react-icons/hi";
 import { BiSolidHome } from "react-icons/bi";
+import { SiGooglehome } from "react-icons/si";
 import { IoPeople } from "react-icons/io5";
+import { BsPersonVideo2 } from "react-icons/bs";
 import { RiSuitcaseFill } from "react-icons/ri";
+import { MdOutlineWork } from "react-icons/md";
 import { AiFillMessage } from "react-icons/ai";
+import { BiSolidMessageAlt } from "react-icons/bi";
 import { GoBellFill } from "react-icons/go";
 import { FaCaretDown } from "react-icons/fa";
 import { CgMenuGridR } from "react-icons/cg";
@@ -31,7 +35,7 @@ const Header = () => {
           <div className="flex border-r border-[#4b4f56]">
             <div className="flex flex-col items-center mr-6 min-w-[60px] max-w-[80px] relative">
               <div className="">
-                <BiSolidHome className="size-6" />
+                <SiGooglehome className="size-6" />
               </div>
               <div className="absolute bg-white rounded-full top-[2px] left-[30px]">
                 <FaDotCircle className="text-red-600 size-[14px] animate" />
@@ -40,9 +44,9 @@ const Header = () => {
             </div>
             <div className="flex flex-col items-center mr-6 min-w-[60px] max-w-[80px] relative">
               <div className="">
-                <IoPeople className="size-6 scale-x-[-1]" />
+                <BsPersonVideo2 className="size-6 scale-x-[-1]" />
               </div>
-              <div className="absolute size-[14px] bg-red-600 rounded-full top-[2px] left-[32px] flex justify-center items-center text-[11px]">
+              <div className="absolute size-[14px] bg-red-600 rounded-full top-[2px] left-[38px] flex justify-center items-center text-[11px]">
                 1
               </div>
               <div className="text-[11px] pt-0.5 display-6">
@@ -51,13 +55,13 @@ const Header = () => {
             </div>
             <div className="flex flex-col items-center mr-6 min-w-[60px] max-w-[80px]">
               <div className="">
-                <RiSuitcaseFill className="size-6" />
+                <MdOutlineWork className="size-6" />
               </div>
               <div className="text-[11px] pt-0.5">Jobs</div>
             </div>
             <div className="flex flex-col items-center mr-6 min-w-[60px] max-w-[80px]">
               <div className="">
-                <AiFillMessage className="size-6" />
+                <BiSolidMessageAlt className="size-6" />
               </div>
               <div className="text-[11px] pt-0.5">Messaging</div>
             </div>
