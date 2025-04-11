e1749c2ffd745da90785082e48a43782dfbc8aad
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-11 22:02:50 +0530

    changes

--- src/Subcomponents/SidebarComponents/LanguagesKnownComponent.jsx
+++ src/Subcomponents/SidebarComponents/LanguagesKnownComponent.jsx
@@ -8,7 +8,7 @@ const LanguagesKnownComponent = () => {
   return (
     <>
       <div className="bg-[#1b1f23] h-fit w-full rounded-xl ml-2 mt-5 relative">
-        <div className="px-4 py-4">
+        <div className="px-4 pt-5 pb-4">
           <div className="text-[#c6c7c8] flex items-center justify-between">
             <h1 className="text-white text-[18px] font-semibold">Languages</h1>
             <div className="flex items-center gap-3">
@@ -16,10 +16,14 @@ const LanguagesKnownComponent = () => {
               <MdOutlineModeEditOutline className="size-4" />
             </div>
           </div>
-          <div className="">
+          <div className="mt-4">
             <div className="border-b border-[#373a3d] text-white pb-3">
-              <h1 className=" font-semibold mt-3">English</h1>
-              <p className="text-sm">Professional working proficiency</p>
+              <h1 className=" font-semibold ">English</h1>
+              <p className="text-[0.8rem]">Professional working proficiency</p>
+            </div>
+            <div className=" text-white">
+              <h1 className=" font-semibold mt-3">Tamil</h1>
+              <p className="text-[0.8rem]">Native or bilingual proficiency</p>
             </div>
           </div>
         </div>
