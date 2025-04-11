136a6a4cc8127886c88c4219dd25ba9549bf5309
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-11 21:58:30 +0530

    chnges

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ import { SiFurrynetwork } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <div className='w-full h-16 bg-[#1b1f23] flex items-center justify-center rounded-t-xl mx-2'>
-      <div className="text-white text-center text-sm flex items-center justify-center gap-4">
+    <div className='w-full h-16 bg-[#1b1f23] flex items-center justify-center rounded-t-xl'>
+      <div className="text-white text-center flex items-center justify-center gap-4">
         <SiFurrynetwork className='size-4 animate-spin' />
         <p className='text-base font-medium opacity-75'>Panda Productions</p>
       </div>
