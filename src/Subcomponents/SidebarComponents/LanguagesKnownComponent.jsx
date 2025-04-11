584aaa450b38d145e38e9f7b1e39f0142fd59171
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-11 20:35:21 +0530

    language tab

---
+++ src/Subcomponents/SidebarComponents/LanguagesKnownComponent.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const LanguagesKnownComponent = () => {
+  return (
+    <div>LanguagesKnownComponent</div>
+  )
+}
+
+export default LanguagesKnownComponent
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,12 +2,14 @@
 import React from 'react'
 
 import LanguageComponent from '../Subcomponents/SidebarComponents/LanguageComponent';
+import LanguagesKnownComponent from '../Subcomponents/SidebarComponents/LanguagesKnownComponent';
 
 const SideBar = () => {
   return (
     <div>
       
         <LanguageComponent />
+        <LanguagesKnownComponent />
     </div>
   );
 }
