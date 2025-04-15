b7fe827f6ca7695f8db793a483fff0d05e93b1be
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-15 18:17:03 +0530

    url component change

--- src/Subcomponents/SidebarComponents/UrlComponent.jsx
+++ src/Subcomponents/SidebarComponents/UrlComponent.jsx
@@ -8,17 +8,7 @@ const UrlComponent = () => {
     <>
       <div className="bg-[#1b1f23] h-fit w-full rounded-xl ml-2 mt-5">
         <div className="px-4 py-4 ">
-          <div className="border-b-2 border-[#373a3d] pb-4 relative">
-            <div className="absolute text-[#c6c7c8] top-0 right-0">
-              <MdOutlineModeEditOutline />
-            </div>
-            <div className="text-[#e9e9e9]">
-              <h1 className="text-[18px] font-semibold">Profile language</h1>
-              <p className="text-[14px] pt-0.5 text-[#8d8f91]">English</p>
-            </div>
-          </div>
-
-          <div className="pt-4 relative">
+          <div className=" relative">
             <div className="absolute text-[#c6c7c8] top-4 right-0">
               <MdOutlineModeEditOutline />
             </div>
