835eab4c004ac4a2b5f784f906b9050de1c6f46d
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-15 18:17:45 +0530

    remove edit button - url component

--- src/Subcomponents/SidebarComponents/UrlComponent.jsx
+++ src/Subcomponents/SidebarComponents/UrlComponent.jsx
@@ -1,17 +1,13 @@
 
 import React from 'react'
 
-import { MdOutlineModeEditOutline } from "react-icons/md";
 
 const UrlComponent = () => {
   return (
     <>
       <div className="bg-[#1b1f23] h-fit w-full rounded-xl ml-2 mt-5">
         <div className="px-4 py-4 ">
-          <div className=" relative">
-            <div className="absolute text-[#c6c7c8] top-4 right-0">
-              <MdOutlineModeEditOutline />
-            </div>
+          <div className="">
             <div className="text-[#e9e9e9]">
               <h1 className="text-[18px] font-semibold">
                 Public Profile & URL
