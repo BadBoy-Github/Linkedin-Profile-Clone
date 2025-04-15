24b1c9d1f1fbec75639c5337a886d02864c29eb4
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-15 18:24:13 +0530

    organizations component changes

--- src/Subcomponents/SidebarComponents/OrganizationComponent.jsx
+++ src/Subcomponents/SidebarComponents/OrganizationComponent.jsx
@@ -8,16 +8,12 @@ import { GoDotFill } from "react-icons/go";
 const OrganizationComponent = () => {
   return (
     <>
-      <div className="bg-[#1b1f23] h-fit w-full rounded-xl ml-2 mt-3 relative">
+      <div className="bg-[#1b1f23] h-fit w-full rounded-xl ml-4 mt-4">
         <div className="px-4 pt-5 pb-4">
           <div className="text-[#c6c7c8] flex items-center justify-between">
             <h1 className="text-white text-[18px] font-semibold">
               Organizations
             </h1>
-            <div className="flex items-center gap-3">
-              <FaPlus className="size-4" />
-              <MdOutlineModeEditOutline className="size-4" />
-            </div>
           </div>
           <div className="mt-4 pr-1">
             <div className="text-white pb-3">
@@ -30,9 +26,8 @@ const OrganizationComponent = () => {
                 <p>Jan 2020 - Present</p>
               </div>
               <p className="text-[0.8rem] text-[#8d8f91] pt-1">
-                Developed leadership skills through community projects,
-                networking with young entrepreneurs, and organizing local impact
-                initiatives.
+                Built leadership through community projects, networking with
+                entrepreneurs, and organizing impactful local initiatives.
               </p>
             </div>
           </div>
