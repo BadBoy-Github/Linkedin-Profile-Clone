0c15212129314fa4b15dc9bddbc08a9b492447b5
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-17 22:41:50 +0530

    chages

--- src/Subcomponents/BodyComponents/HighlightComponent.jsx
+++ src/Subcomponents/BodyComponents/HighlightComponent.jsx
@@ -1,10 +1,11 @@
+import React from "react";
 
-import React from 'react'
+import { BiSolidSend } from "react-icons/bi";
 
 const HighlightComponent = () => {
   return (
     <>
-      <div className="bg-[#1b1f23] h-fit w-full rounded-xl mr-2 mt-2 mb-2 pt-7 pb-1 px-3">
+      <div className="bg-[#1b1f23] w-full rounded-xl mr-2 mt-2 mb-2 pt-7 pb-1 px-3 h-fit">
         <div className="text-white px-5">
           <h1 className="font-semibold text-[1.2rem]">Highlights</h1>
         </div>
@@ -20,29 +21,30 @@ const HighlightComponent = () => {
           </p>
         </div>
 
-        <div className="bg-[#38434f] py-2 px-3 mr-4 ml-2 rounded-xl text-white text-sm mt-4 mb-6 flex">
-          <div className="w-[8%] flex items-center justify-center mr-2">
+        <div className="bg-[#38434f] py-2 px-3 mr-4 ml-2 rounded-xl text-white text-sm mt-4 mb-6 flex h-[6rem] relative">
+          <div className="size-[4.6rem] flex items-center justify-center mr-2">
             <img
-              src="../../../public/img/LinkedIn Profile.jpg"
+              src="../../../public/img/KSRCT Logo.jpeg"
               alt="ksr logo"
               className="rounded-xl"
             />
           </div>
-          <div className="">
-            <p className="font-semibold">
+          <div className=" flex flex-col justify-center">
+            <p className="font-semibold text-[0.96rem]">
               Both studied at K.S.Rangasamy College of Technology
             </p>
             <p className="pt-1">
-              You both studied at K.S.Rangasamy College of Technology from 2020 to 2023
+              You both studied at K.S.Rangasamy College of Technology from 2020
+              to 2023
             </p>
-            <p className="text-[#6eb1f3] font-semibold cursor-pointer hover:underline pt-1 w-fit">
-              Show details
-            </p>
+            <button className="absolute bg-[#6eb1f3] hover:bg-[#4aa1f8] active:bg-[#6eb1f3] transition-all duration-300 text-[#38434f] rounded-full p-1 text-base font-semibold flex items-center cursor-pointer top-2 right-2">
+              <BiSolidSend className="size-[100%]" />
+            </button>
           </div>
         </div>
       </div>
     </>
   );
-}
+};
 
-export default HighlightComponent
+export default HighlightComponent;
