2932535665f900fec272665929cb1f165bb091b8
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-21 14:44:10 +0530

    message component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Header from "../src/components/Header";
 import Body from "../src/components/Body";
 import SideBar from "../src/components/SideBar";
 import Footer from "./components/Footer";
+import Messaging from "./components/Messaging";
 
 const App = () => {
   return (
     <>
-      <div className="flex flex-col bg-black min-h-screen w-full items-center">
-        <div className="w-full">
+      <div className="flex flex-col bg-black min-h-screen w-full items-center relative">
+        <div className="w-3/5">
           <Header />
         </div>
         <div className="flex bg-black h-full w-full lg:w-3/5 flex-grow">
@@ -18,8 +19,11 @@ const App = () => {
             <SideBar />
           </div>
         </div>
+        <div className="fixed bottom-0 right-0 z-50 mx-10">
+          <Messaging />
+        </div>
 
-        <div className="w-full">
+        <div className="w-3/5">
           <Footer />
         </div>
       </div>
---
+++ src/components/Messaging.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Messaging = () => {
+  return (
+    <>
+      <div className="bg-[#1b1f23] px-4 py-2 flex items-center rounded-t-xl">
+        dp
+Messaging
+3 dots 
+edit
+up arrow 
+      </div>
+    </>
+  );
+};
+
+export default Messaging;
