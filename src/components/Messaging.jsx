d4a68ffe2cf19fb094b366bab0eac03624860f59
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-21 20:03:03 +0530

    message hidden in small screen

--- src/components/Messaging.jsx
+++ src/components/Messaging.jsx
@@ -9,7 +9,7 @@ import { IoMdArrowDropupCircle } from "react-icons/io";
 const Messaging = () => {
   return (
     <>
-      <div className="bg-[#1b1f23] px-2 py-2 flex items-center rounded-t-xl">
+      <div className="bg-[#1b1f23] px-2 py-2 items-center rounded-t-xl hidden xl:flex">
         <div className="flex px-2 items-center justify-between gap-12">
           <div className="flex items-center gap-2 ">
             <div className="relative p-1">
