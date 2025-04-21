258f24954e0cb93d8c3b4c3ec3a195a62904885f
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-21 18:56:05 +0530

    messaging

--- src/components/Messaging.jsx
+++ src/components/Messaging.jsx
@@ -4,11 +4,17 @@ const Messaging = () => {
   return (
     <>
       <div className="bg-[#1b1f23] px-4 py-2 flex items-center rounded-t-xl">
-        dp
-Messaging
-3 dots 
-edit
-up arrow 
+        <div className="flex px-2 items-center justify-between gap-6">
+          <div className="flex items-center gap-2">
+            <div className="">dp </div>
+            <div className="">Messaging</div>
+          </div>
+          <div className="flex items-center gap-2">
+            <div className="">3 dot </div>
+            <div className="">edit</div>
+            <div className="">up arrow</div>
+          </div>
+        </div>
       </div>
     </>
   );
