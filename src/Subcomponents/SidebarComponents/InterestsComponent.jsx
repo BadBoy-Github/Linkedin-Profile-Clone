230cad02691b65e163e44b7d849f80073601f708
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-21 20:20:57 +0530

    interest component change

--- src/Subcomponents/SidebarComponents/InterestsComponent.jsx
+++ src/Subcomponents/SidebarComponents/InterestsComponent.jsx
@@ -7,16 +7,24 @@ const InterestsComponent = () => {
   return (
     <>
       <div className="bg-[#1b1f23] h-fit w-full rounded-xl ml-4 mt-2">
-        <div className="px-4 pt-5 pb-4">
-          <div className="text-[#c6c7c8] flex items-center justify-between">
+        <div className="pt-5 pb-4">
+          <div className="text-[#c6c7c8] flex items-center justify-between px-4 ">
             <h1 className="text-white text-[18px] font-semibold">Interests</h1>
           </div>
           <div className="my-1">
-            <div className="text-sm flex items-center text-white px-0.5">
-              <div className=" px-0.5">company</div>
-              <div className=" px-0.5">Group</div>
-              <div className=" px-0.5">Newsletter</div>
-              <div className=" px-0.5">School</div>
+            <div className="text-xs flex items-center justify-center text-white gap-1">
+              <div className="px-1 py-0.5 rounded-xl outline outline-green-600 text-green-600 hover:bg-green-600 hover:text-white cursor-pointer">
+                company
+              </div>
+              <div className="px-1 py-0.5 rounded-xl outline outline-green-600 text-green-600 hover:bg-green-600 hover:text-white cursor-pointer">
+                Group
+              </div>
+              <div className="px-1 py-0.5 rounded-xl outline outline-green-600 text-green-600 hover:bg-green-600 hover:text-white cursor-pointer">
+                Newsletter
+              </div>
+              <div className="px-1 py-0.5 rounded-xl outline outline-green-600 text-green-600 hover:bg-green-600 hover:text-white cursor-pointer">
+                School
+              </div>
             </div>
           </div>
         </div>
