046ebd1d147f8d1a0824b2e35bd0e2a942b3b211
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-23 10:26:15 +0530

    skills component change

--- src/Subcomponents/SidebarComponents/SkillsComponent.jsx
+++ src/Subcomponents/SidebarComponents/SkillsComponent.jsx
@@ -1,38 +1,41 @@
 
 import React from 'react'
 
-import { GoDotFill } from "react-icons/go";
+import { FaArrowUp } from "react-icons/fa";
 
 const SkillsComponent = () => {
   return (
     <>
-          <div className="bg-[#1b1f23] h-fit w-full rounded-xl ml-4 mt-2">
-            <div className="px-4 pt-5 pb-4">
-              <div className="text-[#c6c7c8] flex items-center justify-between">
-                <h1 className="text-white text-[18px] font-semibold">
-                  Organizations
-                </h1>
+      <div className="bg-[#1b1f23] h-fit w-full rounded-xl ml-4 mt-2">
+        <div className="px-4 pt-5 pb-4">
+          <div className="text-[#c6c7c8] flex items-center justify-between">
+            <h1 className="text-white text-[18px] font-semibold">Skills</h1>
+          </div>
+          <div className="mt-4 pr-1">
+            <div className="text-white pb-3">
+              <div className="flex items-center justify-between pr-1">
+                <h1 className=" font-semibold ">React JS</h1>
+                <div className="bg-">
+                    <FaArrowUp className='size-[1rem]'/>
+                </div>
               </div>
-              <div className="mt-4 pr-1">
-                <div className="text-white pb-3">
-                  <h1 className=" font-semibold ">JCI - Erode Galaxy</h1>
-                  <div className="flex items-center text-[0.84rem] pt-1">
-                    <p className="">Member</p>
-                    <div className="px-1">
-                      <GoDotFill className="size-[0.4rem]" />
-                    </div>
-                    <p>Jan 2020 - Present</p>
-                  </div>
-                  <p className="text-[0.8rem] text-[#8d8f91] pt-1">
-                    Built leadership through community projects, networking with
-                    entrepreneurs, and organizing impactful local initiatives.
-                  </p>
+              <div className="flex flex-wrap items-center text-[0.84rem] pt-1 w-[90%] bg-red-500 gap-2">
+                <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
+                  Full Stack Dev Intern at Corizo
+                </div>
+                <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
+                  eCommerce Website
+                </div>
+                <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
+                  Spotify Clone
                 </div>
               </div>
             </div>
           </div>
-        </>
-  )
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default SkillsComponent
