d7741a01087681c1ea1576feb7741619e82a4d3f
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-23 10:29:58 +0530

    interesy component tect hover effect

--- src/Subcomponents/SidebarComponents/InterestContentAllComponent.jsx
+++ src/Subcomponents/SidebarComponents/InterestContentAllComponent.jsx
@@ -13,16 +13,20 @@ const InterestContentAllComponent = ({c1name, c1followers, c1img, c2name, c2foll
               <img
                 src={c1img}
                 alt="logo"
-                className="max-h-[2rem] max-w-[2rem] rounded-xl" 
+                className="max-h-[2rem] max-w-[2rem] rounded-xl"
               />
             </div>
           </div>
           <div className="flex flex-col justify-center ml-1">
-            <p className="text-sm font-semibold text-white">{c1name}</p>
-            <p className="text-xs text-gray-400">{c1followers}</p>
+            <p className="text-sm font-semibold text-white hover:text-[#6eb1f3] hover:underline cursor-pointer w-fit">
+              {c1name}
+            </p>
+            <p className="text-xs text-gray-400 w-fit cursor-pointer">
+              {c1followers}
+            </p>
           </div>
           <div className="flex items-center ml-auto">
-            <button className="text-sm bg-[#6eb1f3] text-[#1b1f23] p-1 rounded-full hover:rotate-90 hover:bg-[#4aa1f8] transition-all duration-400">
+            <button className="text-sm bg-[#6eb1f3] text-[#1b1f23] p-1 rounded-full hover:rotate-90 hover:bg-[#4aa1f8] transition-all duration-400 cursor-pointer">
               <IoMdAdd className="size-4" />
             </button>
           </div>
@@ -33,16 +37,20 @@ const InterestContentAllComponent = ({c1name, c1followers, c1img, c2name, c2foll
               <img
                 src={c2img}
                 alt="logo"
-                className="max-h-[2rem] max-w-[2rem] rounded-xl" 
+                className="max-h-[2rem] max-w-[2rem] rounded-xl"
               />
             </div>
           </div>
           <div className="flex flex-col justify-center ml-1">
-            <p className="text-sm font-semibold text-white">{c2name}</p>
-            <p className="text-xs text-gray-400">{c2followers}</p>
+            <p className="text-sm font-semibold text-white hover:text-[#6eb1f3] hover:underline cursor-pointer w-fit">
+              {c2name}
+            </p>
+            <p className="text-xs text-gray-400 w-fit cursor-pointer">
+              {c2followers}
+            </p>
           </div>
           <div className="flex items-center ml-auto">
-            <button className="text-sm bg-[#6eb1f3] text-[#1b1f23] p-1 rounded-full hover:rotate-90 hover:bg-[#4aa1f8] transition-all duration-400">
+            <button className="text-sm bg-[#6eb1f3] text-[#1b1f23] p-1 rounded-full hover:rotate-90 hover:bg-[#4aa1f8] transition-all duration-400 cursor-pointer">
               <IoMdAdd className="size-4" />
             </button>
           </div>
