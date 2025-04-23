9a21d558711b1894cf978092be7c457e47bbdf44
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-23 12:03:17 +0530

    skills component button change

--- src/Subcomponents/SidebarComponents/SkillsComponent.jsx
+++ src/Subcomponents/SidebarComponents/SkillsComponent.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 import { FaArrowUp } from "react-icons/fa";
+import { IoMdArrowRoundForward } from "react-icons/io";
 
 const SkillsComponent = () => {
   return (
     <>
       <div className="bg-[#1b1f23] h-fit w-full rounded-xl ml-4 mt-2">
-        <div className="px-4 pt-5 pb-4">
+        <div className="px-4  pb-3 pt-5">
           <div className="text-[#c6c7c8] flex items-center justify-between">
             <h1 className="text-white text-[18px] font-semibold">Skills</h1>
           </div>
@@ -51,6 +52,14 @@ const SkillsComponent = () => {
               </div>
               <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
                 ···
+              </div>
+            </div>
+          </div>
+          <div className="mt-1 flex  justify-center items-center border-t border-[#373a3d] group cursor-pointer">
+            <div className="mt-2 flex justify-center items-center gap-2 ">
+              <p className="text-sm font-semibold">Show all 56 skills </p>
+              <div className="group-hover:scale-125 group-hover:animate-pulse transition-all duration-400">
+                <IoMdArrowRoundForward className="size-[1rem]" />
               </div>
             </div>
           </div>
