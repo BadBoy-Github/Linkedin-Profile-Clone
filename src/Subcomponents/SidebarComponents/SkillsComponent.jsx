f759673680ec9b26e97afce751d914e82e1c3f1a
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-23 10:44:47 +0530

    skills section

--- src/Subcomponents/SidebarComponents/SkillsComponent.jsx
+++ src/Subcomponents/SidebarComponents/SkillsComponent.jsx
@@ -1,5 +1,4 @@
-
-import React from 'react'
+import React from "react";
 
 import { FaArrowUp } from "react-icons/fa";
 
@@ -11,24 +10,47 @@ const SkillsComponent = () => {
           <div className="text-[#c6c7c8] flex items-center justify-between">
             <h1 className="text-white text-[18px] font-semibold">Skills</h1>
           </div>
-          <div className="mt-4 pr-1">
-            <div className="text-white pb-3">
-              <div className="flex items-center justify-between pr-1">
-                <h1 className=" font-semibold ">React JS</h1>
-                <div className="bg-">
-                    <FaArrowUp className='size-[1rem]'/>
-                </div>
+          <div className="mt-1 pr-1 text-white pb-3">
+            <div className="flex items-center justify-between">
+              <h1 className=" font-semibold text-sm">React JS</h1>
+              <div className="bg-[#6eb1f3] p-1 rounded-full text-[#1b1f23] flex items-center justify-center cursor-pointer hover:scale-110 hover:bg-[#4aa1f8] transition-all duration-400">
+                <FaArrowUp className="size-[1rem]" />
               </div>
-              <div className="flex flex-wrap items-center text-[0.84rem] pt-1 w-[90%] bg-red-500 gap-2">
-                <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
-                  Full Stack Dev Intern at Corizo
-                </div>
-                <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
-                  eCommerce Website
-                </div>
-                <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
-                  Spotify Clone
-                </div>
+            </div>
+            <div className="flex flex-wrap items-center text-[0.84rem] pt-1 gap-2">
+              <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
+                Full Stack Dev Intern at Corizo
+              </div>
+              <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
+                eCommerce Website
+              </div>
+              <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
+                Spotify Clone
+              </div>
+              <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
+                ···
+              </div>
+            </div>
+          </div>
+          <div className="mt-1 pr-1 text-white pb-3">
+            <div className="flex items-center justify-between">
+              <h1 className=" font-semibold text-sm">Tailwind CSS</h1>
+              <div className="bg-[#6eb1f3] p-1 rounded-full text-[#1b1f23] flex items-center justify-center cursor-pointer hover:scale-110 hover:bg-[#4aa1f8] transition-all duration-400">
+                <FaArrowUp className="size-[1rem]" />
+              </div>
+            </div>
+            <div className="flex flex-wrap items-center text-[0.84rem] pt-1 gap-2">
+              <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
+                Full Stack Dev Intern at Corizo
+              </div>
+              <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
+                eCommerce Website
+              </div>
+              <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
+                Spotify Clone
+              </div>
+              <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
+                ···
               </div>
             </div>
           </div>
@@ -36,6 +58,6 @@ const SkillsComponent = () => {
       </div>
     </>
   );
-}
+};
 
-export default SkillsComponent
+export default SkillsComponent;
