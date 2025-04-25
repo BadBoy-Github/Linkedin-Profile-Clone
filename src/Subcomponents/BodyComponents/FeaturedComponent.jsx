dee0b1d1423ca2a056329b2aa9c68427313074cf
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-25 16:44:17 +0530

    featured component

--- src/Subcomponents/BodyComponents/FeaturedComponent.jsx
+++ src/Subcomponents/BodyComponents/FeaturedComponent.jsx
@@ -7,10 +7,38 @@ const FeaturedComponent = () => {
         <div className="text-white px-5">
           <h1 className="font-semibold text-[1.2rem]">Featured</h1>
         </div>
-        <div className="flex justify-between items-center w-full h-90">
-          <div className="w-full bg-green-400 h-full ml-6"></div>
-          <div className="w-full bg-green-400 h-full mx-3"></div>
-          <div className="w-full bg-green-400 h-full mr-6"></div>
+        <div className="flex justify-between items-center w-full h-[36vh] ">
+          <div className="w-full h-full mx-4 ">
+            <div className="h-full w-full border border-[#38434f] rounded-xl relative ">
+              <div className="bg-[#38434f] shadow-2xl rounded-full absolute flex justify-center items-center px-2 py-0.5 top-2 left-2">
+                <p className="text-xs  text-white">Link</p>
+              </div>
+              <div className="w-fit h-fit flex justify-center items-center rounded-t-xl border-b border-[#38434f]">
+                <img src="/img/DP.jpg" alt="" className="rounded-t-xl size-[16rem]" />
+              </div>
+              <div className="w-full h-full bg-red-400">Hi</div>
+            </div>
+          </div>
+          <div className="w-full h-full mx-4">
+            <div className="h-full w-full border border-[#38434f] rounded-xl relative">
+              <div className="bg-[#38434f] shadow-2xl rounded-full absolute flex justify-center items-center px-2 py-0.5 top-2 left-2">
+                <p className="text-xs  text-white">Link</p>
+              </div>
+              <div className="flex justify-center items-center rounded-t-xl border-b border-[#38434f]">
+                <img src="/img/GFG.jpg" alt="" className="rounded-t-xl" />
+              </div>
+            </div>
+          </div>
+          <div className="w-full h-full mr-6">
+            <div className="h-full w-full border border-[#38434f] rounded-xl relative">
+              <div className="bg-[#38434f] shadow-2xl rounded-full absolute flex justify-center items-center px-2 py-0.5 top-2 left-2">
+                <p className="text-xs  text-white">Post</p>
+              </div>
+              <div className="flex justify-center items-center rounded-t-xl border-b border-[#38434f]">
+                <img src="/img/LIUW.jpg" alt="" className="rounded-t-xl" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
