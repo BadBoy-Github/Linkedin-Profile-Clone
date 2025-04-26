14a2a140ea7d0ac5c7e1f77e390f483abf55d84a
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-26 15:23:47 +0530

    skill component added one skill

--- src/Subcomponents/SidebarComponents/SkillsComponent.jsx
+++ src/Subcomponents/SidebarComponents/SkillsComponent.jsx
@@ -55,7 +55,26 @@ const SkillsComponent = () => {
               </div>
             </div>
           </div>
-        
+          <div className="mt-1 pr-1 text-white pb-3">
+            <div className="flex items-center justify-between">
+              <h1 className=" font-semibold text-sm">JavaScript</h1>
+              <div className="bg-[#6eb1f3] p-1 rounded-full text-[#1b1f23] flex items-center justify-center cursor-pointer hover:scale-110 hover:bg-[#4aa1f8] transition-all duration-400">
+                <FaArrowUp className="size-[1rem]" />
+              </div>
+            </div>
+            <div className="flex flex-wrap items-center text-[0.84rem] pt-1 gap-2">
+              <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
+                Full Stack Dev Trainee at Corizo
+              </div>
+              <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
+                Frontend Shopping List App
+              </div>
+              <div className="flex items-center justify-center text-[0.6rem] px-2 py-0.5 bg-[#38434f] rounded-xl whitespace-nowrap">
+                ···
+              </div>
+            </div>
+          </div>
+
           <div className="mt-1 flex  justify-center items-center border-t border-[#373a3d] group cursor-pointer">
             <div className="mt-2 flex justify-center items-center gap-2 ">
               <p className="text-sm font-semibold">Show all 56 skills </p>
