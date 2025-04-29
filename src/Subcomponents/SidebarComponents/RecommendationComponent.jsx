08ec4a7245c975e67293a974a76b291671e68bd6
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-29 14:34:03 +0530

    changes

--- src/Subcomponents/SidebarComponents/RecommendationComponent.jsx
+++ src/Subcomponents/SidebarComponents/RecommendationComponent.jsx
@@ -1,7 +1,10 @@
 
-import React from 'react'
+import React, {useState} from 'react'
 
 const RecommendationComponent = () => {
+
+  const [showFullText, setShowFullText] = useState(false);
+
   return (
     <>
       <div className="bg-[#1b1f23] h-fit w-full rounded-xl ml-4 mt-2">
@@ -11,31 +14,52 @@ const RecommendationComponent = () => {
               Recommendations
             </h1>
           </div>
-          <div className="mt-4 pr-1">
+          <div className="mt-4 pr-1 relative">
             <div className="text-white pb-3">
               <h1 className=" font-semibold ">Hero</h1>
               <div className="flex items-center text-[0.84rem] pt-1">
                 <p className="">Biotechnologist | Medical Coder</p>
               </div>
-              <p className="text-[0.8rem] text-[#8d8f91] pt-1">
-                It’s rare to come across someone as intellectually curious and
-                driven as Elayabarathi M V. With a strong foundation in both
-                biotechnology and computer science, he bridges the gap between
-                biology and tech effortlessly. His relentless curiosity—always
-                asking what, why, when, and how—fuels his ability to learn and
-                innovate at an impressive pace.<br></br>
-                <br></br> I had the privilege of collaborating with him on an
-                anti-cancer research project, where his analytical mindset and
-                problem-solving skills were invaluable. Beyond that, his solo
-                work in frontend development speaks for itself—he builds
-                stunning, user-friendly websites with a keen eye for detail and
-                functionality.<br></br>
-                <br></br> What sets him apart is his passion for understanding
-                systems deeply and his eagerness to take on new challenges.
-                Whether in the lab or behind a screen, he thrives on turning
-                ideas into reality. Any team would be lucky to have him, and I
-                have no doubt he’ll achieve great things ahead.
-              </p>
+              <div className="text-[0.84rem] text-[#8d8f91] pt-1 ">
+                {showFullText ? (
+                  <>
+                    It’s rare to come across someone as intellectually curious
+                    and driven as Elayabarathi M V. With a strong foundation in
+                    both biotechnology and computer science, he bridges the gap
+                    between biology and tech effortlessly. His relentless
+                    curiosity—always asking what, why, when, and how—fuels his
+                    ability to learn and innovate at an impressive pace.
+                    <br></br>
+                    <br></br> I had the privilege of collaborating with him on
+                    an anti-cancer research project, where his analytical
+                    mindset and problem-solving skills were invaluable. Beyond
+                    that, his solo work in frontend development speaks for
+                    itself—he builds stunning, user-friendly websites with a
+                    keen eye for detail and functionality.<br></br>
+                    <br></br> What sets him apart is his passion for
+                    understanding systems deeply and his eagerness to take on
+                    new challenges. Whether in the lab or behind a screen, he
+                    thrives on turning ideas into reality. Any team would be
+                    lucky to have him, and I have no doubt he’ll achieve great
+                    things ahead.
+                  </>
+                ) : (
+                  <>
+                    It’s rare to come across someone as intellectually curious
+                    and driven as Elayabarathi M V. With a strong foundation in
+                    both biotechnology and computer science, he bridges the gap
+                    between biology and tech effortlessly. His relentless
+                    curiosity—always asking what, why, when, and how—fuels his
+                    ability to learn and innovate at an impressive pace.
+                  </>
+                )}
+                <span
+                  className="text-blue-400 rounded-full cursor-pointer w-fit text-center absolute bottom-0 right-2 p-2 text-xs font-bold"
+                  onClick={() => setShowFullText(!showFullText)}
+                >
+                  {showFullText ? "▲" : "▼"}
+                </span>
+              </div>
             </div>
           </div>
         </div>
