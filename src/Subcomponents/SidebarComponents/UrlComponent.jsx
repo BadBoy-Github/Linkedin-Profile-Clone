fa58d395b44150a707b2eac3479d17aad8414cf1
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-05-20 10:52:25 +0530

    added copy function to url component linkedin link

--- src/Subcomponents/SidebarComponents/UrlComponent.jsx
+++ src/Subcomponents/SidebarComponents/UrlComponent.jsx
@@ -1,8 +1,26 @@
-import React from "react";
 
+import { useState } from "react";
+
+import { IoCopy } from "react-icons/io5";
 import { LuExternalLink } from "react-icons/lu";
 
 const UrlComponent = () => {
+
+  const [isCopied, setIsCopied] = useState(false);
+  const link = "www.linkedin.com/in/elayabarathi";
+
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(link)
+      .then(() => {
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 3000);
+      })
+      .catch((err) => {
+        console.error("Failed to copy text: ", err);
+      });
+  };
+
   return (
     <>
       <div className="bg-[#1b1f23] h-fit w-full rounded-xl ml-4 mt-5">
@@ -10,9 +28,24 @@ const UrlComponent = () => {
           <div className="border-b border-[#4b4f56] pb-2">
             <div className="text-[#e9e9e9]">
               <h1 className="text-[18px] font-semibold">Profile URL</h1>
-              <p className="text-[14px] pt-0.5 text-[#8d8f91]">
-                www.linkedin.com/in/elayabarathi
-              </p>
+              <div className="flex relative text-[#8d8f91] ">
+                <a
+                  href="/"
+                  className="text-[14px] pt-0.5 font-medium hover:text-white hover:underline cursor-pointer"
+                >
+                  {link}
+                </a>
+                <div className="absolute right-0 top-1 cursor-pointer hover:text-[#6eb1f3]">
+                  <div className="relative">
+                    <IoCopy onClick={handleCopy} />
+                    {isCopied && (
+                      <div className="absolute -top-7 -right-2 bg-[#3f3f3f] shadow-lg text-white text-xs px-2 py-1 rounded">
+                        Copied!
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <a
