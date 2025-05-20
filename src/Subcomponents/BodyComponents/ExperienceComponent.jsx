e748f3771e6be33fcb2367629da862d4b2303753
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-05-20 11:06:46 +0530

    managing template for experience component

--- src/Subcomponents/BodyComponents/ExperienceComponent.jsx
+++ src/Subcomponents/BodyComponents/ExperienceComponent.jsx
@@ -6,39 +6,12 @@ const ExperienceComponent = () => {
     <>
       <div className="bg-[#1b1f23] h-fit w-full rounded-xl mr-2 mt-2 mb-2 pt-7 px-3">
         <div className="text-white px-5">
-          <h1 className="font-semibold text-[1.2rem]">About</h1>
+          <h1 className="font-semibold text-[1.2rem]">Experience</h1>
         </div>
 
-        <div className="text-white px-5 text-[0.94rem] pt-4 pb-12 relative">
-          <p>
-            As a dedicated <b>biotechnologist</b> 🧪 with a strong foundation in{" "}
-            <b>microbiology, genetics, and plant biotechnology</b> 🌿, I thrive
-            at the intersection of <b>science and technology</b>. My expertise
-            spans advanced laboratory techniques, data analysis, and innovative
-            research methodologies, allowing me to tackle real-world biological
-            challenges with precision ⚡.
-            <br /> <br />
-            Beyond the lab, I am a <b>tech enthusiast</b> with a passion for
-            <b> frontend web development</b>. I craft clean, responsive, and
-            user-friendly websites using modern technologies like{" "}
-            <b>HTML, CSS, JavaScript, React and Tailwind CSS</b>. My ability to
-            merge <b>analytical thinking</b>
-            (from biotech) with <b>creative problem-solving</b> (from coding)
-            enables me to build intuitive digital experiences.
-            <br /> <br />
-            Driven by continuous learning 🧠, I am always expanding my skill
-            set—whether in <b>biotech research</b> or <b>web development</b>.
-            I’m excited about opportunities where science and technology
-            converge, from bioinformatics to science communication through
-            interactive web platforms.
-            <br /> <br />
-            Let’s connect 🤝 and explore how we can collaborate—whether in
-            <b>
-              {" "}
-              biotech innovation, scientific web solutions, or cutting-edge
-              research!
-            </b>{" "}
-          </p>
+        <div className="text-white px-5 text-[0.94rem] pt-4 pb-12 w-full bg-red-600 flex">
+            <div className="w-1/8 bg-green-600 p-1">Logo</div>
+            <div className="w-7/8 bg-blue-600 p-1">Details</div>
         </div>
       </div>
     </>
