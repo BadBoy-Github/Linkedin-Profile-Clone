b47f68c29d99f05f8414b62e9b2a8c969087ed24
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-05-20 17:46:23 +0530

    created experienc card to use it in the experience component as a template

---
+++ src/Subcomponents/BodyComponents/ExperienceCardComponent.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const ExperienceCardComponent = () => {
+  return (
+    <div>ExperienceCardComponent</div>
+  )
+}
+
+export default ExperienceCardComponent
--- src/Subcomponents/BodyComponents/ExperienceComponent.jsx
+++ src/Subcomponents/BodyComponents/ExperienceComponent.jsx
@@ -1,4 +1,4 @@
-import { LiaIndustrySolid } from "react-icons/lia";
+
 
 const ExperienceComponent = () => {
   return (
