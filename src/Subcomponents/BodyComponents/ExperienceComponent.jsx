1dce05b0f763768184381fc3b2d5575a5df4b4b3
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-05-22 14:03:22 +0530

    changes

--- src/Subcomponents/BodyComponents/ExperienceComponent.jsx
+++ src/Subcomponents/BodyComponents/ExperienceComponent.jsx
@@ -2,13 +2,14 @@ import ExperienceIndividualComponent from "./ExperienceIndividualComponent";
 
 const experience = [
   {
+    imgSrc: "/img/Experience/corizo.jpg",
     name: "Corizo",
     position: "Full Stack Dev Intern",
     worktype: "Remote",
     category: "Internship",
     duration: "Nov 2024 - Dec 2024 · 1 month",
     description:
-      "Completed a comprehensive full stack internship, gaining valuable coding skills!<br></br><br></br> <b>Scope of Internship:</b><br></br>1. Mastered front-end and back-end development techniques.<br></br> 2. Built dynamic web applications from scratch.<br></br>3. Learned industry best practices for coding, debugging, andtesting.<br></br> 4. Worked on real-world projects, enhancing myproblem-solving abilities.<br></br><br></br> Projects Completed:<br></br> 1. Full stack CRUD app with create, read, update, and delete applications user management app.<br></br>2. eCommerce website with all basic features.<br></br><br></br> This internship has been an incredible journey, significantly boosting my knowledge and confidence in the field ofsoftware development. Ready to apply these skills to future projects!",
+      "Completed a comprehensive full stack internship, gaining valuable coding skills!<br></br> <b>Scope of Internship:</b><br></br>1. Mastered front-end and back-end development techniques.<br></br> 2. Built dynamic web applications from scratch.<br></br>3. Learned industry best practices for coding, debugging, andtesting.<br></br> 4. Worked on real-world projects, enhancing myproblem-solving abilities.<br></br> <b>Projects Completed:</b><br></br> 1. Full stack CRUD app with create, read, update, and delete applications user management app.<br></br>2. eCommerce website with all basic features.<br></br> This internship has been an incredible journey, significantly boosting my knowledge and confidence in the field ofsoftware development. Ready to apply these skills to future projects!",
     skills: "React JS, Tailwind CSS and +7 skills",
   },
 ];
@@ -20,7 +21,18 @@ const ExperienceComponent = () => {
         <div className="text-white px-5">
           <h1 className="font-semibold text-[1.2rem]">Experience</h1>
         </div>
-        <ExperienceIndividualComponent />
+        {experience.map((item) => (
+          <ExperienceIndividualComponent
+            imgSrc={item.imgSrc}
+            name={item.name}
+            position={item.position}
+            worktype={item.worktype}
+            category={item.category}
+            duration={item.duration}
+            description={item.description}
+            skills={item.skills}
+          />
+        ))}
       </div>
     </>
   );
--- src/Subcomponents/BodyComponents/ExperienceIndividualComponent.jsx
+++ src/Subcomponents/BodyComponents/ExperienceIndividualComponent.jsx
@@ -1,59 +1,51 @@
 import { GiCutDiamond } from "react-icons/gi";
 
-const ExperienceIndividualComponent = () => {
+const ExperienceIndividualComponent = ({
+  imgSrc,
+  name,
+  position,
+  worktype,
+  category,
+  duration,
+  description,
+  skills,
+}) => {
   return (
     <>
       <div className="text-white px-5 text-[0.94rem] pt-4 pb-12 w-full flex">
-        <div className="w-1/8 p-1">
+        <div className="w-1/10 p-1">
           <div className="">
             <img
-              src="/img/Experience/corizo.jpg"
+              src={imgSrc}
               alt="Corizo Logo"
-              className="size-16 rounded-xl"
+              className="size-12 rounded-xl"
             />
           </div>
         </div>
-        <div className="w-7/8 p-1 ml-1">
+        <div className="w-full p-1">
           <div className="flex flex-col">
-            <h1 className="font-semibold text-lg">Corizo</h1>
+            <h1 className="font-semibold text-lg">{name}</h1>
           </div>
           <div className="mt-1">
             <div className="flex gap-2 items-center">
-              <h1 className="font-semibold text-base">Full Stack Dev Intern</h1>
+              <h1 className="font-semibold text-base">{position}</h1>
               <p className="bg-[#38434f] px-1 py-0.5 rounded-xl text-xs ">
-                Remote
+                {worktype}
               </p>
             </div>
-            <p className="text-sm ">Internship</p>
-            <p className="text-[#999b9d] text-xs">
-              Nov 2024 - Dec 2024 · 1 month
-            </p>
+            <p className="text-sm ">{category}</p>
+            <p className="text-[#999b9d] text-xs">{duration}</p>
           </div>
-          <div className="mt-2">
-            Completed a comprehensive full stack internship, gaining valuable
-            coding skills!<br></br>
-            <br></br> <b>Scope of Internship:</b>
-            <br></br>
-            1. Mastered front-end and back-end development techniques.
-            <br></br> 2. Built dynamic web applications from scratch.<br></br>{" "}
-            3. Learned industry best practices for coding, debugging, and
-            testing.<br></br> 4. Worked on real-world projects, enhancing my
-            problem-solving abilities.<br></br>
-            <br></br> Projects Completed:<br></br> 1. Full stack CRUD app with
-            create, read, update, and delete applications user management app.
-            <br></br>
-            2. eCommerce website with all basic features.<br></br>
-            <br></br> This internship has been an incredible journey,
-            significantly boosting my knowledge and confidence in the field of
-            software development. Ready to apply these skills to future
-            projects!
-          </div>
+          <div
+            className="mt-6"
+            dangerouslySetInnerHTML={{ __html: description }}
+          />
           <div className="mt-6 flex items-center gap-3 cursor-pointer group">
-            <div className="group-hover:text-yellow-500 transition-all duration-300">
+            <div className="group-hover:text-yellow-500">
               <GiCutDiamond />
             </div>
             <p className="font-semibold group-hover:text-[#6eb1f3] group-hover:underline ">
-              React JS, Tailwind CSS and +7 skills
+              {skills}
             </p>
           </div>
         </div>
